330911ee9302e9f5481c50e31215915b1e833bcd
developerashok <ashokchodipalli1@gamil.com>
2023-11-07 14:00:17 +0530

    added simple.py

---
+++ simple.py
@@ -0,0 +1,16 @@
+#write a python program to calculate a simple interest
+
+#The simple formula for simple interest is I = P * T * R / 100
+# I = Interest
+# P = Principal amount
+# T = Time Period
+# R = Rate of Interest
+# take R = 10%
+
+principal_amount = int(input())
+time_period = int(input())
+rate_of_interest = 10/100
+interest = principal_amount * time_period * rate_of_interest
+
+print("The simple interest is : " + str(interest))
+
